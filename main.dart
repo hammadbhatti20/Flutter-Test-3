df95789711a0c8a3737fc90085c1699f6f7c3518
hammadbhatti20 <hammadaslam1052000@gamil.com>
2024-03-09 14:25:02 +0500

    ASSIGNMENT # 3 complete

---
+++ main.dart
@@ -0,0 +1,56 @@
+//Q1
+// import 'dart:io';
+
+// void main() {
+//   // Create two integer variables 'length' and 'breadth' and assign values
+//   int length = 10;  // You can assign any integer value to 'length'
+//   int breadth = 5;  // You can assign any integer value to 'breadth'
+
+//   // Check if the values represent a square or a rectangle
+//   if (length == breadth) {
+//     print("It's a square.");
+//   } else {
+//     print("It's a rectangle.");
+//   }
+// }
+
+//Q2
+// import 'dart:io';
+
+// void main() {
+//   // Prompt the user to enter two ages
+//   print("Enter the first age:");
+//   int age1 = int.parse(stdin.readLineSync()!);
+
+//   print("Enter the second age:");
+//   int age2 = int.parse(stdin.readLineSync()!);
+
+//   // Determine the oldest and youngest ages
+//   if (age1 == age2) {
+//     print("Both ages are equal.");
+//   } else if (age1 > age2) {
+//     print("The oldest age is: $age1");
+//     print("The youngest age is: $age2");
+//   } else {
+//     print("The oldest age is: $age2");
+//     print("The youngest age is: $age1");
+//   }
+// }
+
+//Q3
+// import 'dart:io';
+
+// void main() {
+//   // Prompt the user to enter a number
+//   print("Enter a number:");
+//   double number = double.parse(stdin.readLineSync()!);
+
+//   // Check if the number is positive, negative, or zero
+//   if (number > 0) {
+//     print("The number is positive.");
+//   } else if (number < 0) {
+//     print("The number is negative.");
+//   } else {
+//     print("The number is zero.");
+//   }
+// }
